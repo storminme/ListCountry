ec381812bc462018115e241c2296f81aaf500567
storminme <akimovvlad@icloud.com>
2025-01-13 15:04:00 +0300

    final

--- src/сomponents/pages/countryList/AnimatedDeleteButton.tsx
+++ src/сomponents/pages/countryList/AnimatedDeleteButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { ErrorModal } from '@/сomponents/ErrorModal';
+import { ErrorModal } from '@/сomponents/common/ErrorModal';
 
 type AnimatedDeleteButtonProps = {
   onDeleteAction: () => Promise<void>;
